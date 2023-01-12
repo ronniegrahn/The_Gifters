00efb517e9a7139bcd0df44ed4584750f031ee24
Magnus Åhmansson <magnusahmansson@gmail.com>
2023-01-12 11:30:49 +0100

    Create user is up and running. However, the username is the FirstName in the dbo.AspNetUsers table as of yet.

--- The_Gifters/Controllers/UsersController.cs
+++ The_Gifters/Controllers/UsersController.cs
@@ -1,33 +1,60 @@
 ﻿using Microsoft.AspNetCore.Mvc;
 using The_Gifters.Models;
+using The_Gifters.Views.Users;
 
 namespace The_Gifters.Controllers
 {
-	public class UsersController : Controller
-	{
+    public class UsersController : Controller
+    {
         private readonly UsersService usersService;
 
         public UsersController(UsersService usersService)
-		{
+        {
             this.usersService = usersService;
         }
 
-		[HttpGet("")]
-		public IActionResult Index()
-		{
-			return View();
-		}
+        [HttpGet("")]
+        public IActionResult Index()
+        {
+            return View();
+        }
 
-		[HttpGet("users/login")]
-		public IActionResult Login()
-		{
-			return View();
-		}
+        [HttpGet("users/login")]
+        public IActionResult Login()
+        {
+            return View();
+        }
 
-		[HttpGet("users/create")]
-		public IActionResult Create()
-		{
-			return View();
-		}
-	}
+        [HttpGet("users/create")]
+        public IActionResult Create()
+        {
+            return View();
+        }
+
+        [HttpPost("users/create")]
+        public async Task<IActionResult> CreateAsync(CreateVM viewModel)
+        {
+            if (!ModelState.IsValid)
+                return View();
+
+            // Try to register user
+            var errorMessage = await usersService.TryRegisterAsync(viewModel);
+
+            if (errorMessage != null)
+            {
+                // Show error
+                ModelState.AddModelError(string.Empty, errorMessage);
+                return View();
+            }
+
+            //// This is my own test of 'errorMessage'
+            //if (errorMessage != null)
+            //{
+            //    return Content("Something went wrong!");
+            //}
+
+            return RedirectToAction(nameof(Login));
+            //return View();
+        }
+    }
 }
--- The_Gifters/Models/UsersService.cs
+++ The_Gifters/Models/UsersService.cs
@@ -1,12 +1,60 @@
-﻿using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
+﻿using Microsoft.AspNetCore.Identity;
+using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
+using The_Gifters.Views.Users;
 
 namespace The_Gifters.Models
 {
     public class UsersService
     {
-        public UsersService(IdentityDbContext identityDbContext)
+        UserManager<IdentityUser> userManager;
+        SignInManager<IdentityUser> signInManager;
+
+        public UsersService(IdentityDbContext identityDbContext, UserManager<IdentityUser> userManager, SignInManager<IdentityUser> signInManager)
         {
             identityDbContext.Database.EnsureCreated();
+            this.userManager = userManager;
+            this.signInManager = signInManager;
         }
+
+        public async Task<string> TryRegisterAsync(CreateVM viewModel)
+        {
+            // Todo: Try to create a new user
+            IdentityUser identityUser = new IdentityUser
+            {
+                UserName = viewModel.FirstName,
+                Email = viewModel.Email, 
+                PhoneNumber = viewModel.PhoneNumber,
+            };
+
+            IdentityResult createResult = await
+                userManager.CreateAsync(identityUser, viewModel.Password);
+
+            bool createSucceeded = createResult.Succeeded;
+
+            if (createSucceeded)
+                return null;
+            else
+                return createResult.Errors.First().Description;
+        }
+        
+        //public async Task<string> TryRegisterAsync(CreateVM viewModel)
+        //{
+        //    // Todo: Try to create a new user
+        //    IdentityUser identityUser = new IdentityUser
+        //    {
+        //        UserName = viewModel.FirstName,
+        //        Email = viewModel.LastName
+        //    };
+
+        //    IdentityResult createResult = await
+        //        userManager.CreateAsync(identityUser, viewModel.Password);
+
+        //    bool createSucceeded = createResult.Succeeded;
+
+        //    if (createSucceeded)
+        //        return null;
+        //    else
+        //        return createResult.Errors.First().Description;
+        //}
     }
 }
