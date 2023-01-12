213efbdfec90cd2d3994eca9775c056d56c60630
Sebastian Engh <enghseb@gmail.com>
2023-01-12 09:25:53 +0100

    Completed

---
+++ The_Gifters/Models/ParticipateService.cs
@@ -0,0 +1,14 @@
+﻿using The_Gifters.Models.Entities;
+
+namespace The_Gifters.Models
+{
+	public class ParticipateService
+	{
+		private readonly GiftersContext giftersContext;
+
+		public ParticipateService(GiftersContext giftersContext)
+		{
+			this.giftersContext = giftersContext;
+		}
+	}
+}
