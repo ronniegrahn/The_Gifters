75e3827a0ba129c3f364d5c7c8535b2841ec585b
jwh1550 <j0861550@gmail.com>
2023-01-18 11:59:26 +0100

    Litefix

--- The_Gifters/Views/Participations/ParticipateVM.cs
+++ The_Gifters/Views/Participations/ParticipateVM.cs
@@ -1,9 +1,12 @@
 ﻿using Microsoft.AspNetCore.Mvc.Rendering;
+using System.ComponentModel.DataAnnotations;
 
 namespace The_Gifters.Views.Participations
 {
 	public class ParticipateVM
 	{
+		[Required(ErrorMessage = "Unable to donate 0")]
+		[Range(0.01, double.MaxValue)]
 		public decimal Amount { get; set; }
 		public SelectListItem[]? OrganizationNames { get; set; }
 		public int SelectedOrganizationName { get; set; }
